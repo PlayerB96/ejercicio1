f8f5633fb74f26cee4251fcd35013489af7b57a5
Bryan <player.b.96@hotmail.com>
2021-12-24 09:54:37 -0500

    f

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,4 +1,7 @@
 import json
+import numpy as np
+import matplotlib.pyplot as plt
+
 from datetime import datetime
 
 def cargar_datos(ruta):
@@ -8,67 +11,78 @@ def cargar_datos(ruta):
         return cursos
 
 if __name__ == '__main__':
+            dt3 = 'años.json'
             dt2 = 'fechas.json'
             dt = 'fechacte.json'
             fechascte = cargar_datos(dt)
             fechas = cargar_datos(dt2)
+            # y = cargar_datos(dt3)
+
             # print(fechascte["fechacte"])
 
             # data=[]
-            a=0;
-            b=0;
-            sumap=0;
-            sumadmacn=0;
-            item1="30-01-2018";
+           
+            # item1="30-11-2020";
             # item2="31-12-2021";
-# for item1 in fechas["fechas"]:
-
-for item2 in fechascte["fechacte"]:
-            data1=datetime.strptime(item1, "%d-%m-%Y")
-            data2=datetime.strptime(item2, "%d-%m-%Y")
-            diferencia = data2-data1
-            dias=diferencia.days
-            # print(dias)
-            dt=dias/365.3
-            dtaños=round(dt, 2)
-            # print(dtaños)
-
-            
-            if(item2=="31-12-2021"):
-                b=1045;
-                sm=a+b;
-
-            if(item2 != "31-12-2021"):
-
-                b=45;
-                sm=a+b;
-
-            # print(sm)
-
-            if(dtaños>=0):
-                denominador=(1.055)**dtaños
-                numerador=sm
-                valor=numerador/denominador
-                valoractual=round(valor, 3)
-                pvv=dtaños*valoractual
-                pvt=round(pvv, 3)
-                # print(pvt)
-                # print(type(valoractual))
+            x=[2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021];
+            y=[];
+            for item1 in fechas["fechas"]:
+                a=0;
+                b=0;
+                sumap=0;
+                sumadmacn=0;
                 
-            if(dtaños<0): 
-                # valor=0;
-                pvt=0.0;
-                valoractual=0;
-                
-                # print(pvt)
-            
-            sumap=sumap+valoractual;
-            preciosucio=round(sumap, 3);
-sumadmacn=(sumadmacn+pvt)/preciosucio;
-        
+                for item2 in fechascte["fechacte"]:
+                        data1=datetime.strptime(item1, "%d-%m-%Y")
+                        data2=datetime.strptime(item2, "%d-%m-%Y")
+                        diferencia = data2-data1
+                        dias=diferencia.days
+                        # print(dias)
+                        dt=dias/365.3
+                        dtaños=round(dt, 2)
+                        # print(dtaños)
 
-print(preciosucio)
-print(sumadmacn)
+                        
+                        if(item2=="31-12-2021"):
+                            b=1045;
+                            sm=a+b;
+
+                        if(item2 != "31-12-2021"):
+
+                            b=45;
+                            sm=a+b;
+
+                        # print(sm)
+
+                        if(dtaños>=0):
+                            denominador=(1.055)**dtaños
+                            numerador=sm
+                            valor=numerador/denominador
+                            valoractual=round(valor, 3)
+                            pvv=dtaños*valoractual
+                            pvt=round(pvv, 3)
+                            # print(pvt)
+                            # print(type(valoractual))
+                            
+                        if(dtaños<0): 
+                            # valor=0;
+                            pvt=0.0;
+                            valoractual=0;
+                            
+                            # print(pvt)
+                        
+                        sumap=sumap+valoractual;
+                        preciosucio=round(sumap, 3);
+                sumadmacn=round(((sumadmacn+pvt)/preciosucio),3);
+                        
+                # print(preciosucio)
+                # print(sumadmacn)
+                y.append(preciosucio)
+            print(x)
+            print(y)
+            # plt.plot(x,y)
+            # plt.show()
+
 
 
 # navidad = datetime.strptime("2021-12-25", "%Y-%m-%d")
